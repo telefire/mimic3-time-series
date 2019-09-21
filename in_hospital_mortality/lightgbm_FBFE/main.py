3acffbc7d5a5abd3bb86d63d9802b0a250572fc9
yanke <huyanke@gmail.com>
2019-09-21 17:17:41 -0500

    adjust

---
+++ in_hospital_mortality/lightgbm_FBFE/main.py
@@ -0,0 +1,134 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import numpy as np
+import argparse
+import os
+import imp
+import re
+
+import preprocessing
+from utils.readers import InHospitalMortalityReader
+
+from utils.preprocessing import Discretizer, Normalizer
+from utils import metrics
+from utils import common_utils
+
+import time
+import json
+
+import xgboost as xgb
+import lightgbm as lgb
+
+from utils.metrics import print_metrics_binary
+from in_hospital_mortality.preprocessing import save_results
+from sklearn.preprocessing import Imputer, StandardScaler
+
+parser = argparse.ArgumentParser()
+#common_utils.add_common_arguments(parser)
+parser.add_argument('--data', type=str, help='Path to the data of decompensation task',
+                    default=os.path.join(os.path.dirname(__file__), '../../../data/in-hospital-mortality/'))
+parser.add_argument('--output_dir', type=str, help='Directory relative which all output files are stored',
+                    default='.')
+parser.add_argument('--timestep', type=float, help='1.0 or 0.8 or 2.0', default='1.0')
+
+args = parser.parse_args()
+print(args)
+
+
+train_reader = InHospitalMortalityReader(dataset_dir=os.path.join(args.data, 'train'),
+                                    listfile=os.path.join(args.data, 'train_listfile.csv'))
+val_reader = InHospitalMortalityReader(dataset_dir=os.path.join(args.data, 'train'),
+                                  listfile=os.path.join(args.data, 'val_listfile.csv'))
+
+test_reader = InHospitalMortalityReader(dataset_dir=os.path.join(args.data, 'test'),
+                                    listfile=os.path.join(args.data, 'test_listfile.csv'))
+
+discretizer = Discretizer(timestep=args.timestep,
+                          store_masks=True,
+                          impute_strategy='previous',
+                          start_time='zero')
+
+
+discretizer_header = discretizer.transform(train_reader.read_example(0)["X"])[1].split(',')
+cont_channels = [i for (i, x) in enumerate(discretizer_header) if x.find("->") == -1]
+
+normalizer = Normalizer(fields=cont_channels)  # choose here which columns to standardize
+
+
+normalizer_state = 'ihm_ts{}.input_str:previous.start_time:zero.normalizer'.format(args.timestep)
+normalizer_state = os.path.join(os.path.dirname(__file__), normalizer_state)
+normalizer.load_params(normalizer_state)
+
+# Read data
+train_raw = preprocessing.load_data(train_reader, discretizer, normalizer)
+val_raw = preprocessing.load_data(val_reader, discretizer, normalizer)
+test_raw = preprocessing.load_data(test_reader, discretizer, normalizer)
+
+# Prepare training
+
+print("==> training")
+
+print("train_x ", train_raw[0].shape)
+print("train_y ", len(train_raw[1]))
+print("val ", len(val_raw[1]))
+print("test ", len(test_raw[1]))
+
+print("result", sum(train_raw[1]) + sum(val_raw[1]) + sum(test_raw[1]))
+
+train_raw_reshape = train_raw[0].reshape(17939, 48*76)
+val_raw_reshape = val_raw[0].reshape(3222, 48*76)
+test_raw_reshape = test_raw[0].reshape(3236, 48*76)
+
+print('Imputing missing values ...')
+imputer = Imputer(missing_values=np.nan, strategy='mean', axis=0, verbose=0, copy=True)
+imputer.fit(train_raw_reshape)
+train_raw_reshape = np.array(imputer.transform(train_raw_reshape), dtype=np.float32)
+val_raw_reshape = np.array(imputer.transform(val_raw_reshape), dtype=np.float32)
+test_raw_reshape = np.array(imputer.transform(test_raw_reshape), dtype=np.float32)
+
+print('Normalizing the data to have zero mean and unit variance ...')
+scaler = StandardScaler()
+scaler.fit(train_raw_reshape)
+train_raw_reshape = scaler.transform(train_raw_reshape)
+val_raw_reshape = scaler.transform(val_raw_reshape)
+test_raw_reshape = scaler.transform(test_raw_reshape)
+
+
+print("train reshape", train_raw_reshape.shape)
+print("val reshape", val_raw_reshape.shape)
+print("test reshape", test_raw_reshape.shape)
+
+file_name = 'xgboost_raw.'
+
+xgreg = lgb.LGBMRegressor(objective='regression',num_leaves=11,learning_rate=0.07,n_estimators=10000)
+
+eval_set = [(train_raw_reshape, train_raw[1]) , (val_raw_reshape, val_raw[1])]
+
+xgreg.fit(train_raw_reshape, train_raw[1], eval_metric = 'auc,auroc', eval_set = eval_set, verbose = True, early_stopping_rounds = 80)
+
+
+result_dir = os.path.join(args.output_dir, 'results')
+common_utils.create_directory(result_dir)
+
+with open(os.path.join(result_dir, 'train_{}.json'.format(file_name)), 'w') as res_file:
+    ret = print_metrics_binary(train_raw[1], xgreg.predict(train_raw_reshape))
+    ret = {k : float(v) for k, v in ret.items()}
+    json.dump(ret, res_file)
+
+with open(os.path.join(result_dir, 'val_{}.json'.format(file_name)), 'w') as res_file:
+    ret = print_metrics_binary(val_raw[1], xgreg.predict(val_raw_reshape))
+    ret = {k: float(v) for k, v in ret.items()}
+    json.dump(ret, res_file)
+
+time_start = time.time()
+prediction = xgreg.predict(test_raw_reshape)
+time_elapse = time.time() - time_start
+print("Processing time on Test set :", time_elapse, " s")
+
+with open(os.path.join(result_dir, 'test_{}.json'.format(file_name)), 'w') as res_file:
+    ret = print_metrics_binary(test_raw[1], prediction)
+    ret = {k: float(v) for k, v in ret.items()}
+    json.dump(ret, res_file)
+
+
---
+++ in_hospital_mortality/rnn/main.py
@@ -0,0 +1,202 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import numpy as np
+import argparse
+import os
+import imp
+import re
+
+import preprocessing
+from utils.readers import InHospitalMortalityReader
+
+from utils.preprocessing import Discretizer, Normalizer
+from utils import metrics
+from utils import keras_utils
+from utils import common_utils
+
+from keras.callbacks import ModelCheckpoint, CSVLogger
+import time
+
+parser = argparse.ArgumentParser()
+common_utils.add_common_arguments(parser)
+parser.add_argument('--target_repl_coef', type=float, default=0.0)
+parser.add_argument('--deep_supervision', dest='deep_supervision', action='store_true')
+parser.add_argument('--data', type=str, help='Path to the data of decompensation task',
+                    default=os.path.join(os.path.dirname(__file__), '../../data/in-hospital-mortality/'))
+parser.add_argument('--output_dir', type=str, help='Directory relative which all output files are stored',
+                    default='.')
+parser.set_defaults(deep_supervision=False)
+args = parser.parse_args()
+print(args)
+
+if args.small_part:
+    args.save_every = 2**30
+
+target_repl = (args.target_repl_coef > 0.0 and args.mode == 'train')
+
+# Build readers, discretizers, normalizers
+if args.deep_supervision:
+    train_data_loader = common_utils.DeepSupervisionDataLoader(dataset_dir=os.path.join(args.data, 'train'),
+                                                               listfile=os.path.join(args.data, 'train_listfile.csv'),
+                                                               small_part=args.small_part)
+    val_data_loader = common_utils.DeepSupervisionDataLoader(dataset_dir=os.path.join(args.data, 'train'),
+                                                             listfile=os.path.join(args.data, 'val_listfile.csv'),
+                                                             small_part=args.small_part)
+else:
+    train_reader = InHospitalMortalityReader(dataset_dir=os.path.join(args.data, 'train'),
+                                        listfile=os.path.join(args.data, 'train_listfile.csv'))
+    val_reader = InHospitalMortalityReader(dataset_dir=os.path.join(args.data, 'train'),
+                                      listfile=os.path.join(args.data, 'val_listfile.csv'))
+
+discretizer = Discretizer(timestep=args.timestep,
+                          store_masks=True,
+                          impute_strategy='previous',
+                          start_time='zero')
+
+if args.deep_supervision:
+    discretizer_header = discretizer.transform(train_data_loader._data["X"][0])[1].split(',')
+else:
+    discretizer_header = discretizer.transform(train_reader.read_example(0)["X"])[1].split(',')
+cont_channels = [i for (i, x) in enumerate(discretizer_header) if x.find("->") == -1]
+
+normalizer = Normalizer(fields=cont_channels)  # choose here which columns to standardize
+normalizer_state = args.normalizer_state
+if normalizer_state is None:
+    normalizer_state = 'ihm_ts{}.input_str:{}.start_time:zero.normalizer'.format(args.timestep, args.imputation)
+    normalizer_state = os.path.join(os.path.dirname(__file__), normalizer_state)
+normalizer.load_params(normalizer_state)
+
+args_dict = dict(args._get_kwargs())
+args_dict['header'] = discretizer_header
+args_dict['task'] = 'ihm'
+args_dict['target_repl'] = target_repl
+
+# Build the model
+print("==> using model {}".format(args.network))
+model_module = imp.load_source(os.path.basename(args.network), args.network)
+model = model_module.Network(**args_dict)
+suffix = "{}.bs{}{}{}.ts{}".format("" if not args.deep_supervision else ".dsup",
+                                   args.batch_size,
+                                   ".L1{}".format(args.l1) if args.l1 > 0 else "",
+                                   ".L2{}".format(args.l2) if args.l2 > 0 else "",
+                                   args.timestep,
+                                   ".trc{}".format(args.target_repl_coef) if args.target_repl_coef > 0 else ""
+                                   )
+model.final_name = args.prefix + model.say_name() + suffix
+print("==> model.final_name:", model.final_name)
+
+
+# Compile the model
+print("==> compiling the model")
+optimizer_config = {'class_name': args.optimizer,
+                    'config': {'lr': args.lr,
+                               'beta_1': args.beta_1}}
+
+# NOTE: one can use binary_crossentropy even for (B, T, C) shape.
+#       It will calculate binary_crossentropies for each class
+#       and then take the mean over axis=-1. Tre results is (B, T).
+
+if target_repl:
+    loss = ['binary_crossentropy'] * 2
+    loss_weights = [1 - args.target_repl_coef, args.target_repl_coef]
+else:
+    loss = 'binary_crossentropy'
+    loss_weights = None
+
+model.compile(optimizer=optimizer_config,
+              loss=loss,
+              loss_weights = loss_weights)
+model.summary()
+
+# Load model weights
+n_trained_chunks = 0
+if args.load_state != "":
+    model.load_weights(args.load_state)
+    n_trained_chunks = int(re.match(".*epoch([0-9]+).*", args.load_state).group(1))
+
+
+# Read data
+train_raw = preprocessing.load_data(train_reader, discretizer, normalizer, args.small_part)
+val_raw = preprocessing.load_data(val_reader, discretizer, normalizer, args.small_part)
+
+if target_repl:
+    T = train_raw[0][0].shape[0]
+
+    def extend_labels(data):
+        data = list(data)
+        labels = np.array(data[1])  # (B,)
+        data[1] = [labels, None]
+        data[1][1] = np.expand_dims(labels, axis=-1).repeat(T, axis=1)  # (B, T)
+        data[1][1] = np.expand_dims(data[1][1], axis=-1)  # (B, T, 1)
+        return data
+
+    train_raw = extend_labels(train_raw)
+    val_raw = extend_labels(val_raw)
+
+if args.mode == 'train':
+
+    # Prepare training
+    path = os.path.join(args.output_dir, 'keras_states/' + model.final_name + '.epoch{epoch}.test{val_loss}.state')
+
+    metrics_callback = keras_utils.InHospitalMortalityMetrics(train_data=train_raw,
+                                                              val_data=val_raw,
+                                                              target_repl=(args.target_repl_coef > 0),
+                                                              batch_size=args.batch_size,
+                                                              verbose=args.verbose)
+    # make sure save directory exists
+    dirname = os.path.dirname(path)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    saver = ModelCheckpoint(path, verbose=1, period=args.save_every)
+
+    keras_logs = os.path.join(args.output_dir, 'keras_logs')
+    if not os.path.exists(keras_logs):
+        os.makedirs(keras_logs)
+    csv_logger = CSVLogger(os.path.join(keras_logs, model.final_name + '.csv'),
+                           append=True, separator=';')
+
+    print("==> training")
+    model.fit(x=train_raw[0],
+              y=train_raw[1],
+              validation_data=val_raw,
+              epochs=n_trained_chunks + args.epochs,
+              initial_epoch=n_trained_chunks,
+              callbacks=[metrics_callback, saver, csv_logger],
+              shuffle=True,
+              verbose=args.verbose,
+              batch_size=args.batch_size)
+
+elif args.mode == 'test':
+
+    # ensure that the code uses test_reader
+    # ensure that the code uses test_reader
+    del train_reader
+    del val_reader
+    del train_raw
+    del val_raw
+
+    test_reader = InHospitalMortalityReader(dataset_dir=os.path.join(args.data, 'test'),
+                                            listfile=os.path.join(args.data, 'test_listfile.csv'),
+                                            period_length=48.0)
+    ret = preprocessing.load_data(test_reader, discretizer, normalizer, args.small_part,
+                          return_names=True)
+
+    data = ret["data"][0]
+    labels = ret["data"][1]
+    names = ret["names"]
+
+    time_start = time.time()
+    predictions = model.predict(data, batch_size=args.batch_size, verbose=1)
+    time_elapse = time.time() - time_start
+    print("Processing time on Test set :", time_elapse, " s")
+
+
+    predictions = np.array(predictions)[:, 0]
+    metrics.print_metrics_binary(labels, predictions)
+
+    path = os.path.join(args.output_dir, "test_predictions", os.path.basename(args.load_state)) + ".csv"
+    preprocessing.save_results(names, predictions, labels, path)
+
+else:
+    raise ValueError("Wrong value for args.mode")
